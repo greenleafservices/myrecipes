6dca750c13817efb3c719727208840fb8f3538e5
John Stegman <john@green-leaf-services.com>
2017-03-27 22:22:49 -0400

    After Route Setup-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
+  #get 'chefs/index'
+
+  #get 'chefs/show'
+
+  #get 'chefs/new'
+
+  #get 'chefs/edit'
+
+  #get 'chefs/delete'
+  resources :chefs do # default
+    member do
+      get :delete #should be added ,not part of default resources
+    end
+  end
+
+  resources :recipes do # default
+    member do
+      get :delete #should be added ,not part of default resources
+    end
+  end
+
   #root "pages#home"
 	get 'pages/home'
 
---
+++ test/controllers/chefs_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class ChefsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get chefs_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get chefs_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get chefs_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get chefs_edit_url
+    assert_response :success
+  end
+
+  test "should get delete" do
+    get chefs_delete_url
+    assert_response :success
+  end
+
+end
