17224bd6b55bce56b487451f1fa97c25329b7c80
John Stegman <john@green-leaf-services.com>
2017-03-29 22:34:49 -0400

    Chef Display, Edit and Entry

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,16 +1,52 @@
 class ChefsController < ApplicationController
   def index
+    @chefs = Chef.all
   end
 
   def show
+    @chef = Chef.find(params[:id])
   end
 
   def new
+    @chef = Chef.new
+  end
+
+  def create
+    @chef = Chef.new(chef_params)
+
+    if @chef.save
+      flash[:success] = "Chef Profile was created successfully!"
+      redirect_to chef_path(@chef)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @chef = Chef.find(params[:id])
+    if @chef.update(chef_params)
+      flash[:success] = "Chef Profile was updated successfully!"
+      redirect_to chef_path(@chef)
+    else
+      render 'edit'
+    end
   end
 
   def edit
+  @chef = Chef.find(params[:id])
   end
 
   def delete
   end
-end
+
+  def destroy
+    Chef.find(params[:id]).destroy
+    flash[:success] = "Chef Profile was deleted successfully!"
+    redirect_to chefs_path
+  end
+  private
+
+    def chef_params
+      params.require(:chef).permit(:name, :email)
+    end
+  end
